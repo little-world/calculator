b8f3ef6a31a509034a85b8ccc6a842f7bb7dad9e
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2019-10-23 13:52:21 +0200

    16 buttons with labels

--- src/io/littleworld/Calculator.java
+++ src/io/littleworld/Calculator.java
@@ -12,12 +12,16 @@ public class Calculator extends JFrame implements ActionListener {
 
     public Calculator() {
 
+        String[] labels = { "7", "8", "9", "/",
+                            "4", "5", "6", "*",
+                            "1", "2", "3", "+",
+                            "C", "0", "=", "-" };
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(4, 4));
         getContentPane().add(panel, BorderLayout.CENTER);
 
         for(int i = 0; i< 16; i++) {
-            JButton button = new JButton("" + i);
+            JButton button = new JButton(labels[i]);
             panel.add(button);
             button.addActionListener(this);
         }
