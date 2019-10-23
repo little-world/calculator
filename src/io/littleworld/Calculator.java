3cbc49d07640380c2513c534ae6d2547d4ed5dd3
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2019-10-23 13:31:47 +0200

    an empty frame

---
+++ src/io/littleworld/Calculator.java
@@ -0,0 +1,14 @@
+package io.littleworld;
+
+import javax.swing.*;
+
+public class Calculator extends JFrame {
+
+    public static void main(String[] args) {
+	// write your code here
+        Calculator calculator = new Calculator();
+        calculator.setSize(400, 400);
+        calculator.setVisible(true);
+        calculator.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
--- src/io/littleworld/Main.java
+++
@@ -1,8 +0,0 @@
-package io.littleworld;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
