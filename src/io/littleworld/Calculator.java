b4c4c0a5e297c800bbe547a6a7e1dc0584212e73
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2019-10-23 13:38:52 +0200

    add button click

--- src/io/littleworld/Calculator.java
+++ src/io/littleworld/Calculator.java
@@ -2,8 +2,10 @@ package io.littleworld;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Calculator extends JFrame {
+public class Calculator extends JFrame implements ActionListener {
 
     JButton button = new JButton("OK");
     JLabel display = new JLabel("display");
@@ -11,6 +13,8 @@ public class Calculator extends JFrame {
     public Calculator() {
         getContentPane().add(button, BorderLayout.SOUTH);
         getContentPane().add(display, BorderLayout.NORTH);
+
+        button.addActionListener(this);
     }
 
     public static void main(String[] args) {
@@ -19,5 +23,10 @@ public class Calculator extends JFrame {
         calculator.setSize(400, 400);
         calculator.setVisible(true);
         calculator.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        display.setText("clicked");
     }
 }
